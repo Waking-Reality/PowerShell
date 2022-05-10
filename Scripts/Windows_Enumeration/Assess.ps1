0c16440adabc4cc95871ed69a6ba1c38b02857b2
Waking-Reality <69497564+Waking-Reality@users.noreply.github.com>
2022-05-09 20:09:23 -0500

    Update Assess.ps1

--- Scripts/Windows_Enumeration/Assess.ps1
+++ Scripts/Windows_Enumeration/Assess.ps1
@@ -1,21 +1,36 @@
-########## Timeline Analysis ##########
+########## ADS Analysis ##########
 
-Get-ChildItem -Path C:\ -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_.CreationTime -gt ((Get-ChildItem '[PATH-TO-IOC]').CreationTime).AddMinutes(-2) -and $_.CreationTime -lt ((Get-ChildItem '[PATH-TO-IOC]').CreationTime).AddMinutes(2)} | Select-Object FullName,CreationTime
+$MainFile = ".\[FILE]:[STREAM]"
+$ADSFile = "[ADS_FILE]"
+Set-Content $ADSFile -Encoding Byte -Value $MainFile
+Format-Hex $ADSFile | Select-Object -First 10
 
-########## Log Analysis: Privilege Escalation ##########
+########## General Analysis: IOCs ##########
 
-Get-WinEvent -Oldest | Where-Object {$_.id -eq 4728 -or $_.id -eq 4732 -or $_.id -eq 4756} | Select-Object -Last 1  | Select-Object -ExpandProperty message
+$BaselineFolder = "C:\Investigation"
+$ADSBaseline = @(Get-Content "$BaselineFolder\Alternate_Data_Streams\ADS.txt")
+$ARPBaseline = @(Get-Content "$BaselineFolder\ARP_Cache\ARP_Cache.txt")
+$CurrentProcessesBaseline = @(Get-Content "$BaselineFolder\Current_Processes\Current_Processes.txt")
+$CurrentServicesBaseline = @(Get-Content "$BaselineFolder\Current_Services\Current_Services.txt")
+$DNSCacheBaseline = @(Get-Content "$BaselineFolder\DNS_Cache\DNS_Cache.txt")
+$ETCHostsBaseline = @(Get-Content "$BaselineFolder\ETC_Hosts\ETC_Hosts.txt")
+$FilesBaseline = @(Get-Content "$BaselineFolder\Files\File_Names.txt")
+$HKCUBaseline = @(Get-Content "$BaselineFolder\Registry\HKCU-Registry.txt")
+$HKLMBaseline = @(Get-Content "$BaselineFolder\Registry\HKLM-Registry.txt")
+$HKUBaseline = @(Get-Content "$BaselineFolder\Registry\HKU-Registry.txt")
+$LocalGroupsBaseline = @(Get-Content "$BaselineFolder\Local_Groups\Local_Group_Members.txt")
+$LocalUsersBaseline = @(Get-Content "$BaselineFolder\Local_Users\Local_Users.txt")
+$NetworkConnectedProcessesBaseline = @(Get-Content "$BaselineFolder\Network_Connected_Processes\Network_Connected_Process_Names.txt")
+$NetworkStatisticsBaseline = @(Get-Content "$BaselineFolder\Network_Statistics\Network_Statistics.txt")
+$PrefetchFilesBaseline = @(Get-Content "$BaselineFolder\Prefetch_Files\Prefetch_Files.txt")
+$ScheduledTasksBaseline = @(Get-Content "$BaselineFolder\Scheduled_Tasks\Scheduled_Tasks.txt")
+$ShceduledTaskFilesBaseline = @(Get-Content "$BaselineFolder\Scheduled_Tasks\Scheduled_Task_Files.txt")
+$SMBSharesBaseline = @(Get-Content "$BaselineFolder\SMB_Shares\SMB_Shares.txt")
+$TrustedWinRMBaseline = @(Get-Content "$BaselineFolder\Trusted_WinRM\Trusted_WinRM.txt")
 
-########## Log Analysis: Password Changes ##########
+<#
 
-Get-WinEvent -Oldest | Where-Object {$_.id -eq 4723 -or $_.id -eq 4724} | Select-Object -ExpandProperty message
-
-$BaselineFolder = "[PATH]"
-$FileBaseline = @(Get-Content "$BaselineFolder\files.txt")
-$IPBaseline = @(Get-Content "$BaselineFolder\ips.txt")
-$HKCUBaseline = @(Get-Content "$BaselineFolder\reg-hkcu.txt")
-$HKLMBaseline = @(Get-Content "$BaselineFolder\reg-hklm.txt")
-$HKUBaseline = @(Get-Content "$BaselineFolder\reg-hcu.txt")
+########## Finish Writing this Section ##########
 
 foreach ($OtherConnectedHost in $OtherConnectedHosts) {
 
@@ -60,3 +75,17 @@ foreach ($OtherConnectedHost in $OtherConnectedHosts) {
     Write-Host "===== HKLM =====" -BackgroundColor Black -ForegroundColor DarkYellow
     Get-Content C:\Investigation\$OtherConnectedHost\Found\HKLM.txt
 }
+
+#>
+
+########## Log Analysis: Privilege Escalation ##########
+
+Get-WinEvent -Oldest | Where-Object {$_.id -eq 4728 -or $_.id -eq 4732 -or $_.id -eq 4756} | Select-Object -Last 1  | Select-Object -ExpandProperty message
+
+########## Log Analysis: Password Changes ##########
+
+Get-WinEvent -Oldest | Where-Object {$_.id -eq 4723 -or $_.id -eq 4724} | Select-Object -ExpandProperty message
+
+########## Timeline Analysis ##########
+
+Get-ChildItem -Path C:\ -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_.CreationTime -gt ((Get-ChildItem '[PATH-TO-IOC]').CreationTime).AddMinutes(-2) -and $_.CreationTime -lt ((Get-ChildItem '[PATH-TO-IOC]').CreationTime).AddMinutes(2)} | Select-Object FullName,CreationTime
